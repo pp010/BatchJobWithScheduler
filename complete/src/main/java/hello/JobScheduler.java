11423ed14dae2a1673486bd77363042ab8c62b20
pp010 <padarabinda.panigrahi@cognizant.com>
2018-02-08 12:35:33 +0530

    initial

---
+++ complete/src/main/java/hello/JobScheduler.java
@@ -0,0 +1,32 @@
+package hello;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobScheduler {
+
+	@Autowired
+	JobLauncher jobLauncher;
+
+	@Autowired
+	Job job;
+
+	@Scheduled(fixedRate = 600000)
+	public void runJob() throws JobExecutionAlreadyRunningException, JobRestartException,
+			JobInstanceAlreadyCompleteException, JobParametersInvalidException {
+		
+		JobParameters jobParameters = new JobParametersBuilder().toJobParameters();
+		jobLauncher.run(job, jobParameters);
+	}
+
+}
